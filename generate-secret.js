73fd7dd754662a407f3c435447f4abf26beb4a80
Daniel Tomaz <joaodanielgtt@gmail.com>
2025-11-04 18:02:27 -0300

    chore: Remove arquivos de instruÃ§Ã£o e scripts temporÃ¡rios

--- generate-secret.js
+++
@@ -1,9 +0,0 @@
-// Script para gerar NextAuth Secret
-const crypto = require('crypto');
-
-const secret = crypto.randomBytes(32).toString('base64');
-console.log('\n✅ NextAuth Secret gerado:');
-console.log(secret);
-console.log('\n📝 Adicione esta linha ao seu arquivo .env.local:');
-console.log(`NEXTAUTH_SECRET=${secret}\n`);
-
