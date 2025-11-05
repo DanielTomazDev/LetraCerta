40cb43ad14d24da0c4c636ca0d42a8d7469ac6c7
Daniel Tomaz <joaodanielgtt@gmail.com>
2025-11-05 14:17:54 -0300

    fix: rewrites para /api/músicas e /api/musicas apontarem para /api/songs

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  async rewrites() {
+    return [
+      // Evitar problemas com acentos em rotas de API no filesystem
+      { source: '/api/músicas/:path*', destination: '/api/songs/:path*' },
+      { source: '/api/musicas/:path*', destination: '/api/songs/:path*' },
+    ];
+  },
 };
 
 module.exports = nextConfig;
